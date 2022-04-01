f62b77f7e4549adb36ab93ba3b1e037be16d8b94
Priscila Castro <priscilacastroa@gmail.com>
2022-03-31 22:48:33 -0300

    continuação

--- src/viking.js
+++ src/viking.js
@@ -1,25 +1,36 @@
 // Soldier
 class Soldier {
-  constructor(health, strength){
-    this.health = health;
-    this.strength = strength;
-  }
-  attack(){
-    return this.strength
-  }
-
-  receiveDamage(damage){
-    this.health -= damage;
+ constructor (health, strength) {
+  this.health = health
+  this.strength = strength
+}
+  attack() {
+    return this.strength;
   }
 
+receiveDamage(damage) {
+  this.health -= damage;
+}
 }
 
+
 // Viking
-class Viking { 
-   constructor (name, health, strength) {
-    super(health, strength);
+class Viking extends Soldier { 
+  constructor (name, health, strength){
+    super(health, strength );
     this.name = name;
   }
+
+  attack() {
+    return this.strength;
+  }
+
+
+
+  
+}
+
+
 
 // Saxon
 class Saxon {}
